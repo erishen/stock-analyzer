48cac6d59f6f30c5438e7497ff518bb45e6d4402
Erishen <leisun8309@gmail.com>
2026-06-22 09:26:15 +0800

    chore: remove stale investkit_utils copy (use libs/investkit_utils instead)

--- investkit_utils/src/investkit_utils/db/constants.py
+++
@@ -1,7 +0,0 @@
-EXCLUDED_KEYWORDS = ["ST", "st", "*ST", "SST", "S*ST", "退市", "退", "PT", "停牌"]
-
-
-def is_excluded_stock(name: str, exclude_st: bool = True) -> bool:
-    if not exclude_st:
-        return False
-    return any(keyword in name for keyword in EXCLUDED_KEYWORDS)
